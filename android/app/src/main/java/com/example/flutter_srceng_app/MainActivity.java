db5ea3589a0c9f12059fd49e0ca7c967aba09429
Carl-Y <52552476+HH45137@users.noreply.github.com>
2024-01-03 00:36:54 +0800

    Update MainActivity.java

--- android/app/src/main/java/com/example/flutter_srceng_app/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_srceng_app/MainActivity.java
@@ -22,8 +22,7 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
-    public static String srcengDir = null;
-    public static List<String> gameModList = null;
+    public static String srcengDir = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,75 +30,9 @@ public class MainActivity extends FlutterActivity {
 //        setContentView(R.layout.activity_main);
     }
 
-    public void setPath(View view) {
-        srcengDir = "";
-    }
-
-    public void runGame(View view) {
-        if (!isSelectGameRootDir()) {
-            return;
-        }
-
+    public void runGame() {
         Intent intent = new Intent(this, SDLActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
-    }
-
-    private List<String> searchGameMods() {
-        List<String> sl = new ArrayList<>();
-
-        if (srcengDir != null) {
-            File tDir = new File(srcengDir);
-            if (!tDir.isDirectory()) {
-                return null;
-            }
-
-            String[] tDirs = tDir.list();
-            for (String tStr : tDirs) {
-                File tDir2 = new File(tDir + "/" + tStr);
-                if (!tDir2.isDirectory()) {
-                    continue;
-                }
-
-                // Find gameinfo.txt
-                File tGameinfotxt = new File(tDir2 + "/" + "gameinfo.txt");
-                if (tGameinfotxt.exists()) {
-                    sl.add(tDir2.getName());
-                }
-            }
-            if (sl.isEmpty()) {
-                return null;
-            }
-        }
-
-        return sl;
-    }
-
-    private boolean isSelectGameRootDir() {
-
-        if (com.example.flutter_srceng_app.MainActivity.srcengDir == null) {
-            new AlertDialog.Builder(this)
-                    .setTitle("提示")
-                    .setMessage("请选择游戏根目录里的一个文件比如 hl2.exe！")
-                    .setPositiveButton("OK", (dialog, which) -> {
-
-                    })
-                    .show();
-            return false;
-        }
-
-        gameModList = searchGameMods();
-        if (gameModList == null) {
-            new AlertDialog.Builder(this)
-                    .setTitle("提示")
-                    .setMessage("请选择正确的游戏目录")
-                    .setPositiveButton("OK", (dialog, which) -> {
-
-                    })
-                    .show();
-            return false;
-        }
-
-        return true;
     }
 }
