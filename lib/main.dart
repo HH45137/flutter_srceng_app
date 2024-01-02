0514da05480e9a74cc091a68a08c12218da9ef4e
Carl-Y <52552476+HH45137@users.noreply.github.com>
2024-01-03 00:11:34 +0800

    Update

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Source Engine App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      home: const IndexPage(title: 'Source Engine App'),
+    );
+  }
+}
+
+class IndexPage extends StatefulWidget {
+  const IndexPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<IndexPage> createState() {
+    return _IndexPage();
+  }
+}
+
+class _IndexPage extends State<IndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 100,
+          ),
+          buildStartButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildStartButton() {
+    return Align(
+      child: ElevatedButton(
+        onPressed: () async {
+          const platform = const MethodChannel('toJava');
+          String returnValue = await platform.invokeMethod('赵四');
+          print("返回值为：" + returnValue);
+        },
+        child: Text('START'),
+      ),
+    );
+  }
+}
